27493a4111fd29774ca501d20914c3a4862109b4
EienMosu <voodoochild9293@hotmail.com>
2021-08-10 09:17:55 +0200

    data

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,35 +1,36 @@
 import React from "react";
 import "./works.scss";
+import { worksData } from "../../data";
 
 const Works = () => {
   return (
     <div className="works" id="works">
       <div className="slider">
-        <div className="container">
-          <div className="item">
-            <div className="left">
-              <div className="leftContainer">
-                <div className="imgContainer">
-                  <img src="assets/react.png" alt="" />
+        {worksData.map((data) => (
+          <div className="container">
+            <div className="item">
+              <div className="left">
+                <div className="leftContainer">
+                  <div className="imgContainer">
+                    <img src={data.icon} alt="" />
+                  </div>
+                  <h2>{data.title}</h2>
+                  <p>{data.desc}</p>
+                  <a href={data.link}>
+                    <span>Try it</span>
+                  </a>
                 </div>
-                <h2>Title</h2>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
-                  accusamus recusandae magni quis debitis magnam error possimus
-                  voluptas ab illum cumque beatae excepturi illo earum eius
-                  commodi dolores autem sint.
-                </p>
-                <span>Projects</span>
+              </div>
+              <div className="right">
+                <img src={data.img} alt="" />
               </div>
             </div>
-            <div className="right">
-              <img src="https://www.filepicker.io/api/file/tjceKBtBSDOrAWAEx2EC" alt="" />
-            </div>
           </div>
-        </div>
+        ))}
       </div>
-      <img src="assets/arrow.png" alt="" className="arrow left"/>
-      <img src="assets/arrow.png" alt="" className="arrow right"/>
+
+      <img src="assets/arrow.png" alt="" className="arrow left" />
+      <img src="assets/arrow.png" alt="" className="arrow right" />
     </div>
   );
 };
